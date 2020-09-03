f4009574101ed1c169b1f227cca4f8354b3bf4f5
Mike Sampson <mike@sambodata.com>
2020-09-03 22:35:38 +1000

    Update Blender version to 2.90.0

--- packer/install.sh
+++ packer/install.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash
 
-set -x
+set -e
+
+BLEND_VER=2.90.0
 
 mkdir ouput
 
-wget -q https://mirror.clarkson.edu/blender/release/Blender2.83/blender-2.83.2-linux64.tar.xz
+wget -q https://mirror.clarkson.edu/blender/release/Blender${BLEND_VER%.*}/blender-$BLEND_VER-linux64.tar.xz
 
-tar xf blender-2.83.2-linux64.tar.xz
+tar xf blender-$BLEND_VER-linux64.tar.xz
 
 sudo yum -y install libX11 libXi libXxf86vm libXfixes libXrender libglvnd-glx
